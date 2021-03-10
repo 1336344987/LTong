6798bf6782b495077871dc5c24858c4dd6c50db0
Dreamback1222 <70202732+Dreamback1222@users.noreply.github.com>
2021-03-10 14:58:18 +0800

    Update scheduler.js

--- utils/scheduler.js
+++ utils/scheduler.js
@@ -90,7 +90,7 @@ let scheduler = {
             queues.push({
                 taskName: taskName,
                 taskState: 0,
-                willTime: willTime.format('YYYY-MM-DD HH:mm:ss'),
+                willTime: willTime.format('YYYY-MM-DD 00:00:00'),
                 waitTime: waitTime
             })
         }
@@ -292,7 +292,7 @@ let scheduler = {
 
             // 任务执行
             // 多个任务同时执行会导致日志记录类型错误，所以仅在tryRun模式开启多个任务并发执行
-            let concurrency = scheduler.isTryRun ? 2 : 2
+            let concurrency = scheduler.isTryRun ? 1 : 1
             let queue = new PQueue({ concurrency });
             console.info('调度任务中', '并发数', concurrency)
             for (let task of will_tasks) {
@@ -306,8 +306,8 @@ let scheduler = {
                     var st = new Date().getTime();
                     try {
                         if (task.waitTime) {
-                            console.info('延迟执行', task.taskName, task.waitTime, 'seconds')
-                            await new Promise((resolve, reject) => setTimeout(resolve, task.waitTime * 1000))
+                            console.info('延迟执行', task.taskName, 30, 'seconds')
+                            await new Promise((resolve, reject) => setTimeout(resolve, task.waitTime * 3000))
                         }
 
                         let ttt = tasks[task.taskName]
